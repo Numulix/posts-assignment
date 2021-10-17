d8b4c491cf792653834bc7511a800475cb6d46eb
Jovan Babic <jbabic999@gmail.com>
2021-10-17 13:04:39 +0200

    Added router links to comments for post

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Post } from 'src/models';
 import { PostsService } from '../services/posts-service.service';
 
@@ -7,18 +8,24 @@ import { PostsService } from '../services/posts-service.service';
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent implements OnInit {
+export class PostsComponent implements OnInit, OnDestroy {
 
   public posts: Post[] = [];
+  public postsSubscription!: Subscription;
 
   constructor(private postsService: PostsService) { }
+  
 
   ngOnInit(): void {
-    this.postsService.getPosts().subscribe(posts => this.posts = posts);
+    this.postsSubscription = this.postsService.getPosts().subscribe(posts => this.posts = posts);
   }
 
   addPostToStart(post: Post) {
     this.posts.unshift(post);
+  }
+
+  ngOnDestroy(): void {
+    this.postsSubscription.unsubscribe();
   }
 
 }
